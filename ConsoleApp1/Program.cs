108c38b93a8be35dc58a1446a73d5712a473c7d7
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-10-30 20:45:56 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            //Output text to the screen.
+            Console.WriteLine("The first app in beginning C# and .Net! :-o");
+            Console.ReadKey();
+        }
+    }
+}
